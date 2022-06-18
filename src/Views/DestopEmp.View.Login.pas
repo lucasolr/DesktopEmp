931b624986beccbd169473c060db6b3eb0cce0b7
lucasolr <83046557+lucasolr@users.noreply.github.com>
2022-06-17 21:34:25 -0300

    Add
    LOgin

--- src/Views/DestopEmp.View.Login.pas
+++ src/Views/DestopEmp.View.Login.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects, FMX.Effects, FMX.Layouts;
 
 type
   TForm1 = class(TForm)
+    RectFundo: TRectangle;
+    RectLogin: TRectangle;
+    Logo: TImage;
+    ShadowEffect1: TShadowEffect;
+    LayoutLogin: TLayout;
+    LabelLogin: TLabel;
   private
     { Private declarations }
   public
