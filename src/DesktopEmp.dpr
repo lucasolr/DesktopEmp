9189a1e89aba1a197e2bc0f799071893de8794fc
lucasolr <83046557+lucasolr@users.noreply.github.com>
2022-07-14 21:55:20 -0300

    Add
    Add tela produtos

--- src/DesktopEmp.dpr
+++ src/DesktopEmp.dpr
@@ -6,13 +6,13 @@ uses
   DestopEmp.View.Login in 'Views\DestopEmp.View.Login.pas' {Form1},
   DesktopEmp.View.Base in 'Views\DesktopEmp.View.Base.pas' {Form2},
   Infra.Styles in 'infra\Infra.Styles.pas' {InfraStyles},
-  DesktopEmp.View.Principal in 'Views\DesktopEmp.View.Principal.pas' {Form3};
+  DesktopEmp.View.Principal in 'Views\DesktopEmp.View.Principal.pas' {Principal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TPrincipal, Principal);
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TForm2, Form2);
   Application.CreateForm(TInfraStyles, InfraStyles);
---
+++ src/Views/DesktopEmp.View.Produtos.pas
@@ -0,0 +1,27 @@
+unit DesktopEmp.View.Produtos;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.Edit, FMX.Objects;
+
+type
+  TForm3 = class(TForm)
+    Edit1: TEdit;
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+
+end.
